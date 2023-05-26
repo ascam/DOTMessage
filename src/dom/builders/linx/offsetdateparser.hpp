84bb39710795c4088b79806f880f052173a0d1c2
alex <a.dorado@macsa.com>
2023-05-26 15:03:11 +0200

    passes class members to private

--- src/dom/builders/linx/offsetdateparser.hpp
+++ src/dom/builders/linx/offsetdateparser.hpp
@@ -15,6 +15,7 @@ namespace macsa {
 				virtual bool VisitEnter(const tinyxml2::XMLElement& element, const tinyxml2::XMLAttribute* attribute);
 				virtual bool VisitExit( const tinyxml2::XMLElement& element);
 
+			private:
 				OffsetDateMap& _offsetDateMap;
 				std::string _offsetName;
 				struct OffsetDate _offsetDate;
